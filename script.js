e929f29ee3c45cc099831c91b38e276dbd9e6fc1
dkleszczynski <danielkleszczynski@gmail.com>
2018-01-06 13:57:27 +0100

    Coding convention fixes

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function countSeenMovies() {
 	seenCount = 0;
 	
 	moviesData.forEach(function(item) {
-		if (item.seen == 'T') {
+		if (item.seen === 'T') {
 			++seenCount;
 		}			
 	});
@@ -28,12 +28,12 @@ function createMoviesList() {
 	moviesData.forEach(function(item) {
 		let listItem = document.createElement('li');
 		
-		for (let i = 0; i < fields.length; i++) {
+		for (let field of fields) {
 			let div = document.createElement('div');
 			let span = document.createElement('span');
-			let value = document.createTextNode(item[fields[i]] + " ");
+			let value = document.createTextNode(item[field] + " ");
 			
-			span.appendChild(document.createTextNode(fields[i] + ": "));
+			span.appendChild(document.createTextNode(field + ": "));
 			div.appendChild(span);
 			div.appendChild(value);
 			listItem.appendChild(div);
@@ -41,14 +41,14 @@ function createMoviesList() {
 		
 		let image = document.createElement("IMG");
 				
-		if (item.seen == 'T') {
+		if (item.seen === 'T') {
 			image.src = "img/checked.png";
 		} else {
 			image.src = "img/unchecked.png";
 		}
 		
 		image.onclick = function() {
-			if (item.seen == 'F') {
+			if (item.seen === 'F') {
 				item.seen = 'T';
 				seenCount++;
 				image.src = "img/checked.png";
